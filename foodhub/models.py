a9481f975273a24d51e5ca91da83e3fae57607d4
zalakshah786 <oel232ec@students.codeinstitute.net>
2025-01-27 15:06:34 +0000

    cheking first foodhub blog

--- foodhub/models.py
+++ foodhub/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100 ,unique=True)
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone = models.CharField(max_length=15)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.name
--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ if os.path.exists(os.path.join(BASE_DIR, 'env.py')):
 SECRET_KEY = 'django-insecure-6xd2#e=9%+%$z(t^0w6)%cfdi_y@y^ei^x%vz-3&cixbenx8d6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
@@ -90,6 +90,7 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 
 # Add this print statement temporarily to debug
 print("Database URL:", os.environ.get("DATABASE_URL"))
